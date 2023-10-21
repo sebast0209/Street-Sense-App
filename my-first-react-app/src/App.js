3ab729af5f3e4298bcb98163baab421e75b5e11c
redardedlifts <147350082+redardedlifts@users.noreply.github.com>
2023-10-21 22:12:41 +0100

    pretty blue

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,10 +1,7 @@
-// this is app.js
-
 import React, { useState, useRef } from 'react';
 import './App.css';
 import LocationInput from './LocationInput';
 import Map from './Map';
-import logo from './logo.svg';
 
 function App() {
   const mapRef = useRef();
@@ -17,20 +14,6 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <h1>StreetSense</h1>
       <LocationInput updateMap={setSelectedBorough} map={map} />
       <Map ref={mapRef} location={selectedBorough} />

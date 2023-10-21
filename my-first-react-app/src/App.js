6ed02ca8a3966dab298ce97cf828341bc2921657
redardedlifts <147350082+redardedlifts@users.noreply.github.com>
2023-10-21 20:47:53 +0100

    ZOOM

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,16 +1,19 @@
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react'; // Import useState and useRef
 import './App.css';
-import React, { useState, useRef } from 'react';
-import Map from './Map';
-import LocationInput from './LocationInput';
+import LocationInput from './LocationInput'; // Import LocationInput component
+import Map from './Map'; // Import Map component
+import logo from './logo.svg';
 
 function App() {
-  const [map, setMap] = useState(null);
   const mapRef = useRef();
+  const [map, setMap] = useState(null);
 
   const setMapInstance = (googleMap) => {
     setMap(googleMap);
   };
+
+  // Set a location variable here based on user input
+  var location = 'queens'; // You should set this based on the user's input
 
   return (
     <div className="App">
@@ -28,11 +31,11 @@ function App() {
           Learn React
         </a>
       </header>
-      <h1>Interactive Google Map</h1>
-      <LocationInput updateMap={setMapInstance} />
-      <Map ref={mapRef} />
+      <h1>StreetSense</h1>
+      <LocationInput updateMap={setMapInstance} map={map} />
+      <Map ref={mapRef} location={location} />
     </div>
   );
 }
 
-export default App; // This should be the only export default statement
+export default App;
--- my-first-react-app/src/LocationInput.js
+++ my-first-react-app/src/LocationInput.js
@@ -5,7 +5,6 @@ class LocationInput extends Component {
     super();
     this.state = {
       location: '', // Initialize the location state
-      selectedArea: '', // To store the selected area
     };
   }
 
@@ -13,67 +12,36 @@ class LocationInput extends Component {
     this.setState({ location: e.target.value });
   }
 
-  handleAreaChange = (e) => {
-    this.setState({ selectedArea: e.target.value });
-  }
-
   handleSubmit = (e) => {
     e.preventDefault();
-    // Process the location and selected area here and update the map
+    // Process the location here and update the map
     this.processLocation();
   }
-
+  
   processLocation() {
     const location = this.state.location;
-    const selectedArea = this.state.selectedArea;
     const { updateMap } = this.props;
-    const geocoder = new window.google.maps.Geocoder();
-
-    const fullLocation = `${location}, ${selectedArea}, New York, USA`;
-
-    geocoder.geocode({ address: fullLocation }, (results, status) => {
-      if (status === 'OK' && results[0]) {
-        const { location } = results[0].geometry;
-
-        // Center and zoom the map on the geocoded location
-        updateMap((map) => {
-          if (map) {
-            map.setCenter(location);
-            map.setZoom(13); // You can adjust the zoom level as needed
-            new window.google.maps.Marker({
-              map,
-              position: location,
-              title: fullLocation,
-            });
-          }
-        });
-      } else {
-        console.error('Geocode was not successful for the following reason: ' + status);
-      }
-    });
+    
+    if (location.toLowerCase() === 'queens') {
+      console.log('QUEENS');
+      updateMap(this.map);
+       // Use the updateMap function to set the map
+    } else {
+      console.log('ZOOM 2');
+      // Handle other locations or geocoding as needed
+    }
   }
+  
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <input
           type="text"
-          placeholder="Enter a location"
+          placeholder="Where are you going?"
           value={this.state.location}
           onChange={this.handleInputChange}
         />
-        <select
-          value={this.state.selectedArea}
-          onChange={this.handleAreaChange}
-        >
-          <option value="">Select an area</option>
-          <option value="Manhattan">Manhattan</option>
-          <option value="Brooklyn">Brooklyn</option>
-          <option value="Queens">Queens</option>
-          <option value="Bronx">Bronx</option>
-          <option value="Staten Island">Staten Island</option>
-          {/* Add more areas as needed */}
-        </select>
         <button type="submit">Submit</button>
       </form>
     );
